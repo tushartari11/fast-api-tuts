b28ea14119a8ea82d4c9675b394c1d73f612c298
Tushar Tari <tushartari11@gmail.com>
2026-03-15 14:53:02 +0100

    remove api method from  hello api

--- hello-api.py
+++ hello-api.py
@@ -11,7 +11,3 @@ def read_root():
 async def read_item(item_id):
     return {"item_id": item_id}
 
-
-@app.get("/items/{item_id}/details/{detail_id}")
-async def read_item_detail(item_id, detail_id):
-    return {"item_id": item_id, "detail_id": detail_id}
